ae12e0d92dfa0f91f557b63830dda282999c92be
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2026-06-09 19:37:12 -0300

    Create joins.sql

---
+++ joins.sql
@@ -0,0 +1,55 @@
+-- Obtener las categorias de peliculas y sus ganancias
+
+-- Obtener la cantidad de rentas por categoria de pelicula
+
+-- Obtener los idiomas y la cantidad de clientes que alquilaron peliculas para cada idioma
+
+-- Obtener la cantidad de clientes por pais
+
+-- Obtener los clientes, la cantidad de rentas y la cantidad de dinero gastado
+
+-- Obtener la renta mas alta para cada tienda
+
+-- Obtener el promedio de renta por tienda
+
+-- Obtener las 10 rentas mas caras por tienda
+
+-- Obtener los nombres de los clientes y la cantidad de dinero gastado en ambas tiendas
+
+-- Obtener la direccion de cada tienda
+
+-- Obtener el nombre, apellido y direccion de cada cliente
+
+-- Obtener el nombre y apellido del cliente que vive en la ciudad "Apeldoorn"
+
+-- Obtener la categoria de la pelicula "Arabia Dogma"
+
+-- Obtener el nombre y apellido de los actores que participaron en la pelicula "Interview Liaisons"
+
+-- Obtener el nombre del miembro del staff que le rento una copia de la pelicula "Hunchback Imposible" al cliente "Kurt Emmons"
+
+-- Obtener cantidad de veces que el cliente "Vivian Ruiz" rento peliculas
+
+-- Obtener la cantidad de peliculas disponibles en ambas tiendas
+
+-- Obtener todas las ciudades con su pais respectivo
+
+-- Obtener los clientes y el monto total de sus pagos realizados en orden descendente
+
+-- Obtener las categorias y la cantidad de peliculas para cada categoria
+
+-- Obtener el id de los vendedores, su direccion y su primer nombre
+
+-- Obtener los nombres de las peliculas donde actuo el actor "Nick Walhberg"
+
+-- Obtener el id de cada alquiler, el id de la tienda y la direccion de la tienda donde se realizo ese alquiler
+
+-- Obtener la pelicula mas alquilada
+
+-- Obtener el id de cada cliente, la direccion y el id de la tienda a la que accedio
+
+-- Obtener las categorias de peliculas y la cantidad de veces que se rentaron peliculas para cada categoria. Agrupar por nombre de categoria y ordenar por cantidad de rentas de mayor a menor
+
+-- Obtener las categorias de peliculas y las ganancias de rentas de peliculas para cada categoria. Agrupar por categoria y ordenar por suma de categorias de mayor a menor
+
+-- 
