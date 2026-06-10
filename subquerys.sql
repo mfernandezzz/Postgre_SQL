e8cb3f74652dfeea7713ec22d3bdbe42cc5ae382
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2026-06-10 20:52:55 -0300

    Create subquerys.sql

---
+++ subquerys.sql
@@ -0,0 +1,17 @@
+-- ¿Cual es la categoria de la pelicula "Arabia Dogma"?
+
+-- Obtener todos los nombres de peliculas donde hayan participado los actores que participaron en la pelicula con id=14
+
+-- Obtener todos los clientes de los que recibio pagos el miembro del staff con id=1
+
+-- Obtener todas las peliculas con categoria "Action"
+
+-- Obtener todos los clientes de pais numero 10 (country_id = 10)
+
+-- Obtener el id de todos los clientes que han visto peliculas en japones
+
+-- Obtener la cantidad de rentas que se hicieron para las peliculas de categoria "Sci-Fi"
+
+-- Obtener las ganancias que obtuvo la categoria de pelicula "Foreign"
+
+-- Mostrar los nombres de las peliculas en las que actuo el actor "Nick Wahlberg"
