d751b2258af9c45dd2068c33f100bc8d6773d72c
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2026-06-22 16:39:49 -0300

    Add SQL queries for film and customer data retrieval

--- subquerys.sql
+++ subquerys.sql
@@ -27,8 +27,35 @@ WHERE actor.first_name = 'Rock' and actor.last_name = 'Dukakis';
 -- Obtener todos los clientes de los que recibio pagos el miembro del staff con id=1
 
 -- Obtener todas las peliculas con categoria "Action"
+SELECT film.title AS title
+FROM film
+WHERE film_id in (
+	SELECT film_id
+	FROM film_category
+	WHERE category_id = 1  --el id de la categoria action, previamente obtenido con un select
+);
+SELECT film.title
+FROM film
+LEFT JOIN film_category
+ON film.film_id = film_category.film_id
+WHERE category_id in (
+	SELECT category_id
+	FROM film_category
+	WHERE category_id = 1
+);
 
 -- Obtener todos los clientes de pais numero 10 (country_id = 10)
+SELECT customer.first_name, customer.last_name
+FROM customer
+LEFT JOIN address
+ON customer.address_id = address.address_id
+LEFT JOIN city
+ON address.city_id = city.city_id
+WHERE country_id in (
+	SELECT country_id
+	FROM city
+	WHERE country_id = 10
+);
 
 -- Obtener el id de todos los clientes que han visto peliculas en japones
 
