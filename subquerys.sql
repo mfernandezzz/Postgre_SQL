22db4ff0fc95442505a2dd9fb571cdc3af9a7fb9
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2026-06-22 15:15:10 -0300

    Add SQL queries for film and actor retrieval
    Added SQL queries to retrieve films and actors related to a specific film ID and to count films for a specific actor.

--- subquerys.sql
+++ subquerys.sql
@@ -1,6 +1,28 @@
 -- ¿Cual es la categoria de la pelicula "Arabia Dogma"?
 
 -- Obtener todos los nombres de peliculas donde hayan participado los actores que participaron en la pelicula con id=14
+SELECT film.title
+FROM film
+LEFT JOIN film_actor
+ON film.film_id = film_actor.film_id
+WHERE actor_id in (
+	SELECT actor_id
+	FROM film_actor
+	WHERE film_id = 14
+);
+--para verificar esta consulta, se obtienen los actores que participaron en la pelicula con id=14
+SELECT actor.first_name, actor.last_name
+FROM actor
+LEFT JOIN film_actor
+ON actor.actor_id = film_actor.actor_id
+WHERE film_id = 14;
+--luego, obtener la cantidad de peliculas en las que actuo cada actor
+SELECT count(film_id)
+FROM film_actor
+LEFT JOIN actor
+ON film_actor.actor_id = actor.actor_id
+WHERE actor.first_name = 'Rock' and actor.last_name = 'Dukakis';
+
 
 -- Obtener todos los clientes de los que recibio pagos el miembro del staff con id=1
 
